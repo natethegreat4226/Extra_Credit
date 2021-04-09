f135a18765d5399359cdfbba6bb79ca8d90b82d1
natethegreat4226 <nthoms_4226@yahoo.com>
2021-04-08 19:53:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-asxjnu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-asxjnu)
---
+++ src/app/game-control/game-control.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-game-control",
+  templateUrl: "./game-control.component.html",
+  styleUrls: ["./game-control.component.css"]
+})
+export class GameControlComponent implements OnInit {
+  startGame() {}
+  pauseGame() {}
+  constructor() {}
+
+  ngOnInit() {}
+}
