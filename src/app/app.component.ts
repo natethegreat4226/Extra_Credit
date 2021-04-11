8b11877b04ec73f6bb7bcb895b393ccff3896264
natethegreat4226 <nthoms_4226@yahoo.com>
2021-04-11 17:30:59 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,10 @@ export class AppComponent {
   name = "Angular " + VERSION.major;
 
   poNumber: number = 0;
+  gameList: number[] = [];
 
   receiveMessage(msg: number) {
     this.poNumber = msg;
+    this.gameList.push(this.poNumber);
   }
 }
